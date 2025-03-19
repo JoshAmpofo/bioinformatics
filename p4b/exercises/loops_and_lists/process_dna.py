8db7e5942fb09bb9773aeb22b9c4155f30c636c5
JoshAmpofo <joshampofo92@gmail.com>
2025-03-19 19:17:47 +0000

    Organizing everything into dirs

---
+++ p4b/exercises/loops_and_lists/process_dna.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Description: input.txt contains a number of DNA sequences, one per line.
+Each sequence starts with the same 14 base pair fragment - a sequencing adapter that should be removed.
+
+Task: Write a program that will:
+    a. trim this adapter and write cleaned sequences to a new file
+    b. print the length of each sequence to the screen
+"""
+
+# get sequence file
+dna_sequences = open("../exercises and examples/lists_and_loops/exercises/input.txt")
+trimmed_sequences = []
+
+# write a loop to access the DNA sequences
+for dna_line in dna_sequences.readlines():
+    trimmed_sequence = dna_line[15:] # trim the adapter sequence
+    trimmed_sequences.append(trimmed_sequence)
+    dna_sequences.close()
+
+for sequence in trimmed_sequences:
+    new_sequence_file = open("trimmed_sequences.txt", "a") # create a new file to hold the new sequences
+    new_sequence_file.write(sequence) # write trimmed/cleaned sequences to new file
+    
+    # close files
+    new_sequence_file.close()
+    
+    # print length
+    print(f"Length of trimmed sequences: {len(sequence)}")
